aec4a04a6d4a055d4d4d44142947e55d4c397a38
Eric P <ericpp@gmail.com>
2024-06-19 17:06:00 -0500

    Added payment tracker library and updated terminal to use it

---
+++ tracker.js
@@ -0,0 +1,316 @@
+
+function PaymentTracker() {
+    const nostrRelays = ["wss://relay.damus.io", "wss://nos.lol", "wss://relay.nostr.band"]
+
+    this.storedBoosts = new StoredBoosts()
+    this.nostrWatcher = new NostrWatcher(nostrRelays)
+
+    this.filters = {}
+    this.identifiers = []
+    this.listener = null
+    this.lastBoostAt = null
+
+    this.loadBoosts = true
+    this.nostrBoostPkey = null
+    this.nostrZapEvent = null
+
+    this.setFilter = (name, value) => {
+        if ((name == 'before' || name == 'after') && typeof(value) != "number") {
+            value = Math.floor(new Date(value) / 1000)
+        }
+
+        this.filters[name] = value
+
+        if (name == 'after') {
+            this.lastBoostAt = value
+        }
+    }
+
+    this.loadBoosts = (shouldLoad) => {
+        this.loadBoosts = shouldLoad
+    }
+
+    this.setNostrBoostPkey = (pkey) => {
+        this.nostrBoostPkey = pkey
+    }
+
+    this.setNostrZapEvent = (event) => {
+        this.nostrZapEvent = event
+    }
+
+    this.setListener = (listener) => {
+        this.listener = listener
+    }
+
+    this.start = async () => {
+        if (this.loadBoosts) {
+            await this.storedBoosts.load(this.lastBoostAt, (item, old) => {
+                this.add(item, old)
+            })
+        }
+
+        if (this.nostrBoostPkey) {
+            this.nostrWatcher.subscribeBoosts(this.nostrBoostPkey, (item, old) => {
+                if (this.lastBoostAt > item.creation_date) {
+                    return
+                }
+
+                this.add(item, old)
+            })
+        }
+
+        if (this.nostrZapEvent) {
+            this.nostrWatcher.subscribeZaps(this.nostrZapEvent, (item, old) => {
+                this.add(item, old)
+            })
+        }
+    }
+
+    this.add = (payment, old) => {
+        if (!payment.sats || isNaN(payment.sats)) {
+            return // missing sat amount
+        }
+
+        if (this.identifiers.indexOf(payment.identifier) !== -1) {
+            return // already seen
+        }
+
+        if (this.filters.excludePodcasts) {
+            const exclude = this.filters.excludePodcasts.filter(
+                filter => payment.podcast.indexOf(filter) !== -1
+            ).length
+
+            if (exclude) {
+                return
+            }
+        }
+
+        if (this.filters.podcast && this.filters.podcast != payment.podcast) {
+            return
+        }
+
+        if (this.filters.before && this.filters.before < payment.creation_date) {
+            return
+        }
+
+        if (this.filters.after && this.filters.after > payment.creation_date) {
+            return
+        }
+
+        this.listener(payment, old)
+
+        this.identifiers.push(payment.identifier)
+    }
+}
+
+function NostrWatcher(relays) {
+    this.nostrPool = new NostrTools.SimplePool()
+
+    this.nostrNameQueue = {}
+    this.nostrNames = {}
+
+    this.nostrRelays = relays
+
+    this.subscribeBoosts = (nostrPubkey, callback) => {
+        let isOld = true
+        let isOldTimeout = null
+        let self = this
+
+        this.nostrPool.subscribeMany(this.nostrRelays, [{authors: [nostrPubkey]}], {
+            onevent(event) {
+                invoice = JSON.parse(event.content)
+
+                if (isOldTimeout) {
+                    clearTimeout(isOldTimeout)
+                }
+
+                if (isOld) {
+                    // turn off isOld after 5 seconds of inactivity
+                    isOldTimeout = setTimeout(() => {
+                        isOld = false
+                    }, 5000)
+                }
+
+                const boost = invoice.boostagram
+
+                callback({
+                    type: 'boost',
+                    action: boost.action || 'unknown',
+                    identifier: invoice.identifier,
+                    creation_date: invoice.creation_date,
+                    sender_name: boost.sender_name || 'Anonymous',
+                    app_name: boost.app_name || 'Unknown',
+                    podcast: boost.podcast || 'Unknown',
+                    sats: Math.floor(boost.value_msat_total / 1000),
+                    message: boost.message,
+                }, isOld)
+            },
+            oneose() {
+                // h.close()
+            }
+        })
+    }
+
+    this.subscribeZaps = (nostrEvent, callback) => {
+        let isOld = true
+        let isOldTimeout = null
+        let self = this
+
+        this.nostrPool.subscribeMany(this.nostrRelays, [{'#a': [nostrEvent], 'kinds': [9735]}], {
+            async onevent(event) {
+                if (isOldTimeout) {
+                    clearTimeout(isOldTimeout)
+                }
+
+                if (isOld) {
+                    // turn off isOld after 5 seconds of inactivity
+                    isOldTimeout = setTimeout(() => {
+                        isOld = false
+                    }, 5000)
+                }
+
+                // convert tags array into an object
+                const tags = event.tags.reduce((result, tag) => {
+                    const [name, value] = tag
+                    if (!result[name]) result[name] = []
+                    result[name].push(value)
+                    return result
+                }, {})
+
+                // original zap request is encoded as a tag in the zap receipt
+                const zaprequest = JSON.parse(tags.description[0])
+
+                // msats can be calculated from bolt11 request
+                const value_msat_total = self.getMsatsFromBolt11(tags.bolt11[0])
+
+                // batch look up names based on original zap request pubkey
+                const sender_name = await self.getNostrName(zaprequest.pubkey)
+
+                // send back to subscriber
+                callback({
+                    type: 'zap',
+                    action: 'zap',
+                    identifier: event.id,
+                    creation_date: event.created_at,
+                    sender_name: sender_name || 'Anonymous',
+                    app_name: 'Nostr',
+                    podcast: 'Nostr',
+                    sats: Math.floor(value_msat_total / 1000),
+                    message: event.content,
+                }, isOld)
+            },
+            oneose() {
+                // h.close()
+            }
+        })
+    }
+
+    this.getNostrName = (pubkey) => {
+        return new Promise((resolve, reject) => {
+            if (this.nostrNames[pubkey]) {
+                resolve(this.nostrNames[pubkey])
+            }
+            else {
+                if (!this.nostrNameQueue[pubkey]) {
+                    this.nostrNameQueue[pubkey] = []
+                }
+
+                this.nostrNameQueue[pubkey].push(resolve)
+            }
+        })
+    }
+
+    this.getMsatsFromBolt11 = (bolt11) => {
+        const multipliers = {
+            m: 100000000,
+            u: 100000,
+            n: 100,
+            p: 0.1,
+        }
+
+        // msat amount encoded in the first part (e.g. lnbc100n -> 100n -> 100 * 100 = 100,000)
+        let matches = bolt11.match(/^ln\w+?(\d+)([a-zA-Z]?)/)
+
+        if (!matches) {
+            return null // no match
+        }
+
+        // calculate the msats from the number and multiplier
+        return parseInt(matches[1]) * multipliers[matches[2]]
+    }
+
+    // resolve queued nostr pubkeys to names
+    setInterval(async () => {
+        if (!this.nostrNameQueue) return
+
+        let pubkeys = Object.keys(this.nostrNameQueue)
+        if (pubkeys.length === 0) return
+
+        let profiles = await this.nostrPool.querySync(this.nostrRelays, {authors: pubkeys, kinds: [0]})
+
+        profiles.forEach(event => {
+            const profile = JSON.parse(event.content)
+            this.nostrNames[event.pubkey] = profile.display_name || profile.name
+        })
+
+        for (let pubkey of pubkeys) {
+            let resolvers = this.nostrNameQueue[pubkey]
+
+            if (resolvers) {
+                delete this.nostrNameQueue[pubkey]
+
+                resolvers.forEach(resolve => {
+                    resolve(this.nostrNames[pubkey] || null)
+                })
+            }
+        }
+    }, 1000)
+}
+
+function StoredBoosts() {
+
+    this.load = async (after, callback) => {
+        let page = 1
+        let items = 1000
+        let lastBoostAt = null
+
+        while (true) {
+            const query = new URLSearchParams()
+            query.set("page", page)
+            query.set("items", items)
+
+            if (after) {
+                query.set("created_at_gt", after)
+            }
+
+            const result = await fetch(`https://boostboard.vercel.app/api/boosts?${query}`)
+            const boosts = await result.json()
+
+            if (!boosts || boosts.length === 0) {
+                break
+            }
+
+            lastBoostAt = Math.max(lastBoostAt, Math.max(...boosts.map(x => x.creation_date)))
+
+            boosts.forEach(invoice => {
+                const boost = invoice.boostagram
+
+                callback({
+                    type: 'boost',
+                    action: boost.action || 'unknown',
+                    identifier: invoice.identifier,
+                    creation_date: invoice.creation_date,
+                    sender_name: boost.sender_name || 'Anonymous',
+                    app_name: boost.app_name || 'Unknown',
+                    podcast: boost.podcast || 'Unknown',
+                    sats: Math.floor(boost.value_msat_total / 1000),
+                    message: boost.message,
+                }, true)
+            })
+
+            page++
+        }
+
+        return lastBoostAt
+    }
+}
