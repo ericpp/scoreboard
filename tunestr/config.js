b25cfab20d4e0350c43326cdcec1b9379c51fc92
Eric P <ericpp@gmail.com>
2024-12-15 11:31:36 -0600

    Add Independent Music Summit naddr

--- tunestr/config.js
+++ tunestr/config.js
@@ -1,7 +1,7 @@
 function loadConfig() {
   return {
     "nostrBoostPkey": "npub1sp8w4t66l3nu46d22zn2uq6hrtnf8l9jw775p4jtje439h96yh8qzmpw6q",
-    "nostrZapEvent": "naddr1qq9rzdesxvcnzwfjxgcsyg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwensfzsd4h",
+    "nostrZapEvent": "naddr1qq9rzdenxsersvecxquqyg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwens8v5x0v",
     "after": "2024-12-14",
     "excludePodcasts": ["Podcasting 2.0"],
   }
