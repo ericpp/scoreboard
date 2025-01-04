adccf863a7ba746466218864d45e5183c79d71f1
Eric P <ericpp@gmail.com>
2025-01-04 15:28:24 -0600

    Add slots, timing, and custom renderers for alerts

--- alerts.js
+++ alerts.js
@@ -1,13 +1,42 @@
+function escapeHtml(text) {
+  const span = document.createElement("span")
+  span.textContent = text
+  return span.innerHTML
+}
+
 class AlertImage {
   playing = false
   duration = 10
+  currentTime = 0
+  timeHandlers = []
 
   constructor(image) {
     this.image = image
+    this.setTimeInterval()
+  }
+
+  setTimeInterval() {
+    const tickRate = 0.1
+
+    return setInterval(() => {
+      if (!this.playing) return
+
+      this.currentTime = this.currentTime + tickRate
+      this.timeHandlers.forEach(handle => handle(this.currentTime))
+    }, tickRate * 1000)
+  }
+
+  setUrl(url) {
+    this.image.src = url
+  }
+
+  onTimeUpdate(handler) {
+    this.timeHandlers.push(handler)
   }
 
   play() {
     this.playing = true
+    this.currentTime = 0.0
 
     setTimeout(() => {
       this.playing = false
@@ -16,10 +45,31 @@ class AlertImage {
 }
 
 class AlertVideo extends AlertImage {
+  timedEvents = []
+
   constructor(image) {
     super(image)
+
     this.image.addEventListener("play", () => this.playing = true)
     this.image.addEventListener("ended", () => this.playing = false)
+  }
+
+  handleTimeUpdate({ target }) {
+    const currentTime = Math.floor(target.currentTime * 1000)
+    const timedEvent = this.timedEvents[0]
+
+    if (timedEvent && timedEvent.time <= currentTime) {
+      this.timedEvents.push(this.timedEvents.shift())
+      timedEvent.callback()
+    }
+  }
+
+  setUrl(url) {
+    this.image.src = url
+  }
+
+  onTimeUpdate(handler) {
+    this.image.addEventListener("timeupdate", () => handler(this.image.currentTime))
   }
 
   play() {
@@ -27,7 +77,126 @@ class AlertVideo extends AlertImage {
   }
 }
 
-function getAlert(image) {
+class AlertSlot {
+  root = null
+  rootShown = false
+
+  messageRoot = null
+  alert = null
+
+  images = []
+  selectedImage = null
+
+  message = null
+  messageShown = false
+
+  constructor(rootId, options) {
+    options = options || {}
+
+    this.root = document.querySelector(rootId)
+    this.messageRoot = this.root.querySelector(".message")
+
+    this.alert = getAlertObject(this.root.querySelector(".background"))
+    this.images = options.images || null
+
+    this.message = options.message || {}
+
+    this.message.timeShow = this.message.timeShow || 0
+    this.message.timeHide = this.message.timeHide || null
+
+    this.alert.onTimeUpdate(this.handleTimeUpdate.bind(this))
+  }
+
+  pickImage() {
+    if (this.images) {
+      this.selectedImage = Math.floor(Math.random() * this.images.length)
+      this.alert.setUrl(this.images[this.selectedImage])
+    }
+  }
+
+  async setMessage(message, line2, picture) {
+    let textContent = escapeHtml(message)
+
+    if (line2) {
+      text.innerHTML += '<br>' + escapeHtml(line2)
+    }
+
+    await this.setHtmlMessage(message, picture)
+  }
+
+  async setHtmlMessage(message, picture) {
+    const text = this.messageRoot.querySelector(".messageText")
+    text.innerHTML = message
+
+    const box = this.messageRoot.querySelector(".picture")
+    box.innerHTML = ""
+
+    if (picture) {
+      const img = await this.loadPicture(picture)
+      box.appendChild(img)
+    }
+
+    this.messageRoot.style.paddingTop = 0
+    this.messageRoot.style.paddingBottom = 0
+
+    if (this.selectedImage !== null && this.message.offsetY[this.selectedImage] > 0) {
+      this.messageRoot.style.paddingTop = this.message.offsetY[this.selectedImage]
+    }
+
+    if (this.selectedImage !== null && this.message.offsetY[this.selectedImage] < 0) {
+      this.messageRoot.style.paddingBottom = -this.message.offsetY[this.selectedImage]
+    }
+  }
+
+  loadPicture(src) {
+    return new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => resolve(img)
+      img.onerror = reject
+      img.src = src
+      img.style.height = "127px"
+      img.style.width = "auto"
+    })
+  }
+
+  handleTimeUpdate(time) {
+    const messageHide = (this.message.timeHide || this.alert.duration - 2)
+
+    if (!this.messageShown && time >= this.message.timeShow && time < messageHide) {
+      this.messageRoot.classList.add("show")
+      this.messageShown = true
+    }
+
+    if (this.messageShown && time >= messageHide) {
+      this.messageRoot.classList.remove("show")
+      this.messageShown = false
+    }
+
+    if (this.rootShown && time >= this.alert.duration - 2) {
+      this.root.classList.remove("show")
+      this.rootShown = false
+    }
+  }
+
+  playing() {
+    return this.alert.playing
+  }
+
+  play() {
+    this.root.classList.add("show")
+    this.rootShown = true
+    this.alert.play()
+  }
+
+  showMessage(message, line2, picture) {
+    this.pickImage()
+    this.setMessage(message, line2, picture)
+    this.handleTimeUpdate(0)
+    this.play()
+  }
+}
+
+function getAlertObject(image) {
   if (image.tagName == "IMG") {
     return new AlertImage(image)
   }
@@ -47,8 +216,11 @@ async function startAlerts(config) {
 
   const app = new PaymentTracker(config)
 
-  const alert = document.getElementById("alert")
-  const background = getAlert(document.getElementById("background"))
+  const slots = (config.slots || []).map(slot => new AlertSlot(slot.id, slot))
+
+  if (slots.length === 0) {
+    slots.push(new AlertSlot("#alert"))
+  }
 
   const alertQueue = []
 
@@ -75,7 +247,7 @@ async function startAlerts(config) {
       app.loadZaps = true
     }
 
-    if (typeof url.showMessages !== undefined) {
+    if (url.showMessages !== undefined) {
       config.showMessages = url.showMessages
     }
 
@@ -91,31 +263,47 @@ async function startAlerts(config) {
       app.loadZaps = true
     }
 
-    app.setListener((payment, old) => {
-      if (payment.action !== 'boost' && payment.type !== 'zap') {
-        return // filter out streams, basically
-      }
+    setInterval(handleAlertQueue, 1000)
 
-      if (config.priority !== undefined && config.priority !== (payment.creation_date % config.numalerts)) {
-        return // show a porition of the payments based on priority and numalerts
-      }
-
-      alertQueue.push(payment)
-    })
-
-    setInterval(() => {
-      if (alertQueue.length === 0 || background.playing) {
-        return
-      }
-
-      const payment = alertQueue.shift()
-      const sats = payment.sats.toLocaleString()
-      const message = config.showMessages ? payment.message : getRemoteInfo(payment)
-
-      showMessage(`${sats} sat ${payment.type} from ${payment.sender_name}`, message, payment.picture)
-    }, 1000)
-
+    app.setListener(paymentReceived)
     app.start()
+  }
+
+  const paymentReceived = (payment, old) => {
+    if (payment.action !== 'boost' && payment.type !== 'zap') {
+      return // filter out streams, basically
+    }
+
+    if (config.priority !== undefined && config.priority !== (payment.creation_date % config.numalerts)) {
+      return // show a porition of the payments based on priority and numalerts
+    }
+
+    alertQueue.push(payment)
+  }
+
+  const handleAlertQueue = () => {
+    if (alertQueue.length === 0) {
+      return
+    }
+
+    const availableSlots = slots.filter(x => !x.playing())
+
+    if (availableSlots.length === 0) {
+      return
+    }
+
+    const slot = availableSlots[Math.floor(Math.random() * availableSlots.length)]
+
+    const payment = alertQueue.shift()
+    const sats = payment.sats.toLocaleString()
+    const userMessage = config.showMessages ? payment.message : getRemoteInfo(payment)
+
+    if (config.messageRender) {
+      config.messageRender(slot, payment)
+    }
+    else {
+      slot.showMessage(`${sats} sat ${payment.type} from ${payment.sender_name}`, userMessage, payment.picture)
+    }
   }
 
   const getRemoteInfo = (payment) => {
@@ -124,43 +312,6 @@ async function startAlerts(config) {
       }
 
       return `${payment.remote_feed} - ${payment.remote_item}`
-  }
-
-  const loadImage = (src) => {
-    return new Promise((resolve, reject) => {
-      let img = new Image()
-      img.onload = () => resolve(img)
-      img.onerror = reject
-      img.src = src
-      img.style.height = "127px"
-      img.style.width = "auto"
-    })
-  }
-
-  const showMessage = async (message, line2, picture) => {
-    background.play()
-
-    const msg = alert.querySelector("#messageText")
-    msg.textContent = message
-
-    if (line2) {
-      const span = document.createElement("span")
-      span.textContent = line2
-      msg.innerHTML += '<br>' + span.innerHTML
-    }
-
-    if (picture) {
-      const box = alert.querySelector("#picture")
-      box.innerHTML = ""
-      const img = await loadImage(picture)
-      box.appendChild(img)
-    }
-
-    alert.classList.add("show")
-
-    setTimeout(() => {
-      alert.classList.remove("show")
-    }, (background.duration - 2) * 1000)
   }
 
   init()
