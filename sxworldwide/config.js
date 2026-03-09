eba631378bf7f3d1d61bfbd342a7948ae5316ef7
Eric P <ericpp@gmail.com>
2026-03-08 21:06:34 -0500

    Add alerts and config

---
+++ sxworldwide/config.js
@@ -0,0 +1,10 @@
+function loadConfig() {
+  return {
+    "nostrBoostPkey": "npub1sp8w4t66l3nu46d22zn2uq6hrtnf8l9jw775p4jtje439h96yh8qzmpw6q",
+    "nostrZapEvent": "naddr1qq9rzdenxsersvecxquqyg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwens8v5x0v",
+    "after": "2026-01-08 00:00:00 -0500",
+    "podcasts": ["As If: Lit At Boost Beach"],
+    "episodeGuids": ["c6fa7516-3be6-43a2-b5cf-73484baf49dd"],
+    "eventGuids": ["58eb81f8-caeb-4c77-a326-36e406207fe0", "303fe7e4-32c1-4f67-977d-967f251cb6b0"],
+  }
+}
