36c6bea8f452bd112cdeb23a2f07fb6fb6ad25af
Eric P <ericpp@gmail.com>
2026-03-10 21:46:40 -0500

    Update sxworldwide config

--- sxworldwide/config.js
+++ sxworldwide/config.js
@@ -1,10 +1,9 @@
 function loadConfig() {
   return {
     "nostrBoostPkey": "npub1sp8w4t66l3nu46d22zn2uq6hrtnf8l9jw775p4jtje439h96yh8qzmpw6q",
-    "nostrZapEvent": "naddr1qq9rzdenxsersvecxquqyg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwens8v5x0v",
-    "after": "2026-01-08 00:00:00 -0500",
-    "podcasts": ["As If: Lit At Boost Beach"],
-    "episodeGuids": ["c6fa7516-3be6-43a2-b5cf-73484baf49dd"],
-    "eventGuids": ["58eb81f8-caeb-4c77-a326-36e406207fe0", "303fe7e4-32c1-4f67-977d-967f251cb6b0"],
+    "nostrZapNpub": "npub1k2c3h68etzu3atgcypznfw2u9a4cfgm53rnv7p4g7t88ag8eg7zsawndfp",
+    "after": "2026-03-10 00:00:00 -0500",
+    "podcasts": ["SXWorldwide"],
+    "eventGuids": ["b9f6ab8d-8885-4987-bed8-c02fc9ac94ca"],
   }
 }
