db6b582395ffa067310e9d22cc70edfdd9ab401d
Eric P <ericpp@gmail.com>
2026-03-12 12:34:32 -0500

    Switch to spencer's npub for now

--- sxworldwide/config.js
+++ sxworldwide/config.js
@@ -1,7 +1,7 @@
 function loadConfig() {
   return {
     "nostrBoostPkey": "npub1sp8w4t66l3nu46d22zn2uq6hrtnf8l9jw775p4jtje439h96yh8qzmpw6q",
-    "nostrZapNpub": "npub1k2c3h68etzu3atgcypznfw2u9a4cfgm53rnv7p4g7t88ag8eg7zsawndfp",
+    "nostrZapNpub": "npub1yvscx9vrmpcmwcmydrm8lauqdpngum4ne8xmkgc2d4rcaxrx7tkswdwzdu",
     "after": "2026-03-10 00:00:00 -0500",
     "podcasts": ["SXWorldwide"],
     "eventGuids": ["b9f6ab8d-8885-4987-bed8-c02fc9ac94ca"],
