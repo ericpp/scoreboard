1fcb9a7fcafd7e2ba2ff393486ac8431d92a2afc
Eric P <ericpp@gmail.com>
2026-03-12 12:39:33 -0500

    Add BAB to config

--- sxworldwide/config.js
+++ sxworldwide/config.js
@@ -3,7 +3,7 @@ function loadConfig() {
     "nostrBoostPkey": "npub1sp8w4t66l3nu46d22zn2uq6hrtnf8l9jw775p4jtje439h96yh8qzmpw6q",
     "nostrZapNpub": "npub1yvscx9vrmpcmwcmydrm8lauqdpngum4ne8xmkgc2d4rcaxrx7tkswdwzdu",
     "after": "2026-03-10 00:00:00 -0500",
-    "podcasts": ["SXWorldwide"],
+    "podcasts": ["SXWorldwide", "Bowl After Bowl"],
     "eventGuids": ["b9f6ab8d-8885-4987-bed8-c02fc9ac94ca"],
   }
 }
