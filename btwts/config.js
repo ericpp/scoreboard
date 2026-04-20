12e94eaf859bff4ed5780dcfa40c1eb1f1a0db34
Eric P <ericpp@gmail.com>
2026-04-20 00:50:54 -0500

    Add BTWTS alerts

---
+++ btwts/config.js
@@ -0,0 +1,15 @@
+function loadConfig() {
+  return {
+    "nostrBoostPkey": "npub1sp8w4t66l3nu46d22zn2uq6hrtnf8l9jw775p4jtje439h96yh8qzmpw6q",
+    "nostrZapNpubs": [
+      "npub18gp8p84hjmjh9zejdlyqqcg22sj6x3qra0q2ng4v6cxxg95sav8q0mcg5e"
+    ],
+    "after": "2026-04-19 00:00:00 -0500",
+    "podcasts": [
+      "Between The Sch3m3s",
+      "Behind the SchƎmƎs",
+      ""
+    ],
+    "eventGuids": ["c9f4790d-3fd2-4b1b-819d-aa70a976c0c4"],
+  }
+}
